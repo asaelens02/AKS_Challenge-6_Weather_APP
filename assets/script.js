22531197185668f2d2a90ea270657dafb92ec162
asaelens02 <104400626+asaelens02@users.noreply.github.com>
2022-09-18 20:00:45 -0500

    linking html and js files, testing formats

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#cityname");
+var cityContainerEl = document.querySelector("#cities-container");
+var citySearchTerm = document.querySelector("#city-search-term");
